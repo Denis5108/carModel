51ca02a9c96264eb91055f1dc19161a270e6dbfe
Denis Garley <46497648+Denis5108@users.noreply.github.com>
2020-11-15 02:00:43 -0700

    Add files via upload

--- CarModels/src/carModel.java
+++ CarModels/src/carModel.java
@@ -1,12 +1,33 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class carModel {
+    public static ArrayList<String> read() {
+        ArrayList<String> data = new ArrayList<String>();
+        try {
+            File myfile = new File("carFile.txt");
+            Scanner reader = new Scanner(myfile);
+            while (reader.hasNextLine()) {
+                data.add(reader.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred");
+            e.printStackTrace();
+        }
+        return data;
+    }
     public static void write(String data) {
         try {
             FileWriter myWrite = new FileWriter("carFile.txt");
+            if (data.contains("all")) {
+                myWrite.close();
+                System.out.println("Error: '" + data + "' was not recognized");
+                return;
+            }
             myWrite.write(data);
             myWrite.close();
             System.out.println("Successfully wrote the file");
@@ -24,6 +45,7 @@ public class carModel {
         boolean done = false;
         String car;
         ArrayList<String> cars = new ArrayList<String>();
+        cars = read();
 
         while (!done) {
             Scanner in = new Scanner(System.in);
@@ -60,7 +82,19 @@ public class carModel {
             if (choice == 4) {
                 System.out.println("Please type in a car name, type (all) for all cars: ");
                 car = in.next();
-                write(car);
+                if (car.equals("all"))
+                {
+                    for (String i : cars) {
+                        write(i);
+                    }
+                } else {
+                    if (cars.contains(car))
+                    {
+                        write(car);
+                    } else {
+                        System.out.println("Error: Spelling error");
+                    }
+                }
             }
             if (choice == 5) {
                 done = true;
